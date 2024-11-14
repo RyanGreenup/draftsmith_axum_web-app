9e1ceda30764847c25c8b44f0ccd703bbf5c45d7
ryan <exogenesis@protonmail.com>
2024-11-14 15:57:27 +1100

    feat: Update get_static_katex_fonts to serve font files with correct MIME type

--- src/static_files.rs
+++ src/static_files.rs
@@ -53,40 +53,27 @@ pub async fn get_static_files(Path(path): Path<String>) -> impl IntoResponse {
 pub async fn get_static_katex_fonts(Path(path): Path<String>) -> impl IntoResponse {
     let not_found_string: &[u8] = b"Not Found";
 
-    // loop through the fonts directory
-    KATEX_DIR
-        .get_dir("dist/fonts")
-        .unwrap_or_else(|| {
-            panic!("Failed to load fonts directory");
-        })
-        .files()
-        .for_each(|file| {
-            println!("File: {:#?}", file.path());
-        });
+    // Check if the requested path corresponds to a font file
+    if let Some(file) = KATEX_DIR.get_file(format!("dist/fonts/{}", path)) {
+        let content_type = match file.path().extension().and_then(|ext| ext.to_str()) {
+            Some("woff") => "font/woff",
+            Some("woff2") => "font/woff2",
+            Some("ttf") => "font/ttf",
+            Some("otf") => "font/otf",
+            _ => "application/octet-stream",
+        };
 
-    match path.as_str() {
-        "katex.min.js" => (
+        return (
             StatusCode::OK,
-            [(axum::http::header::CONTENT_TYPE, "application/javascript")],
-            KATEX_DIR.get_file("dist/katex.min.js").unwrap().contents(),
-        ),
-        "auto-render.min.js" => (
-            StatusCode::OK,
-            [(axum::http::header::CONTENT_TYPE, "application/javascript")],
-            KATEX_DIR
-                .get_file("dist/contrib/auto-render.min.js")
-                .unwrap()
-                .contents(),
-        ),
-        "katex.min.css" => (
-            StatusCode::OK,
-            [(axum::http::header::CONTENT_TYPE, "text/css")],
-            KATEX_DIR.get_file("dist/katex.min.css").unwrap().contents(),
-        ),
-        _ => (
-            StatusCode::NOT_FOUND,
-            [(axum::http::header::CONTENT_TYPE, "text/plain")],
-            not_found_string,
-        ),
+            [(axum::http::header::CONTENT_TYPE, content_type)],
+            file.contents(),
+        );
     }
+
+    // Return 404 if the file is not found
+    (
+        StatusCode::NOT_FOUND,
+        [(axum::http::header::CONTENT_TYPE, "text/plain")],
+        not_found_string,
+    )
 }
