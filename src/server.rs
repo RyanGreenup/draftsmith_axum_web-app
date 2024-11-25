2d94cde289176f614cfc2ce0163619972fb315e6
Ryan G <exogenesis@protonmail.com>
2024-11-25 15:08:52 +1100

    add footer

--- src/server.rs
+++ src/server.rs
@@ -65,7 +65,7 @@ async fn render_index(api_addr: String, Path(path): Path<i32>) -> Html<String> {
 
     // Load the template
     // TODO don't panic
-    let template = ENV.get_template("body/base.html").unwrap_or_else(|e| {
+    let template = ENV.get_template("body/note/base.html").unwrap_or_else(|e| {
         panic!("Failed to load template. Error: {:#}", e);
     });
 
