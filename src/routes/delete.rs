9acd615c59e2be49aa56380310211459a1ba622a
Ryan G <exogenesis@protonmail.com>
2024-11-29 14:55:07 +1100

    feat: Implement route_delete function for note deletion with flash messages

--- src/routes/delete.rs
+++ src/routes/delete.rs
@@ -1,19 +1,35 @@
-use crate::template_context::{NoteTemplateContext, PaginationParams};
 use axum::{
-    extract::{Path, Query},
-    response::{Html, Redirect},
-    Form,
+    extract::{Path, State},
+    response::{IntoResponse, Response, Redirect},
 };
-
 use crate::flash::{FlashMessage, FlashMessageStore};
 use crate::state::AppState;
-use crate::templates::{handle_not_found, handle_template_error, ENV};
-use axum::{
-    extract::State,
-    response::{IntoResponse, Response},
-};
-use draftsmith_rest_api::client::{UpdateNoteRequest, delete_note};
+use draftsmith_rest_api::client::delete_note;
 use tower_sessions::Session;
 
-pub async fd route_delete(
-    // TODO
+pub async fn route_delete(
+    session: Session,
+    State(state): State<AppState>,
+    Path(id): Path<i32>,
+) -> Response {
+    let api_addr: String = state.api_addr.clone();
+
+    match delete_note(&api_addr, id).await {
+        Ok(_) => {
+            session
+                .set_flash(FlashMessage::success("Note deleted successfully"))
+                .await
+                .unwrap();
+            
+            Redirect::to("/").into_response()
+        }
+        Err(e) => {
+            session
+                .set_flash(FlashMessage::error(format!("Failed to delete note: {}", e)))
+                .await
+                .unwrap();
+            
+            Redirect::to(&format!("/note/{id}")).into_response()
+        }
+    }
+}
